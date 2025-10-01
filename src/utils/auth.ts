cd5e56e4c8ed5a41d078536688358f5b2ee12db2
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-10-01 21:49:33 +0800

    fix: cookie

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -9,7 +9,8 @@ export async function createSession(userId: string) {
   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
   const session = await encrypt({ userId, expiresAt });
 
-  cookies().set("session", session, {
+  const cookieStore = await cookies();
+  cookieStore.set("session", session, {
     httpOnly: true,
     secure: true,
     expires: expiresAt,
@@ -17,7 +18,8 @@ export async function createSession(userId: string) {
 }
 
 export async function deleteSession() {
-  cookies().delete("session");
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
 }
 
 type SessionPayload = {
