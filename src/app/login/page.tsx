a67aeeabd6aa50a1415e6890cef1bb80f25aff6d
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2025-10-01 21:44:44 +0800

    fix: login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,19 @@
 "use client";
 
 import { useActionState } from "react";
-import { useFormStatus } from "react-dom";
-
-// import { login } from "./actions";
+import { login } from "./actions";
 
 export function LoginForm() {
-  const [state, loginAction] = useActionState(login, undefined);
-  const { pending } = useFormStatus();
+  const [state, loginAction, isPending] = useActionState(login, undefined);
 
   return (
     <form action={loginAction} className="flex max-w-[300px] flex-col gap-2">
       <div className="flex flex-col gap-2">
         <input id="email" name="email" placeholder="Email" />
       </div>
-      {/* {state?.errors?.email && (
+      {state?.errors?.email && (
         <p className="text-red-500">{state.errors.email}</p>
-      )} */}
+      )}
 
       <div className="flex flex-col gap-2">
         <input
@@ -26,10 +23,10 @@ export function LoginForm() {
           placeholder="Password"
         />
       </div>
-      {/* {state?.errors?.password && (
+      {state?.errors?.password && (
         <p className="text-red-500">{state.errors.password}</p>
-      )} */}
-      <button disabled={pending} type="submit">
+      )}
+      <button disabled={isPending} type="submit">
         Login
       </button>
     </form>
